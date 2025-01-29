d4f50dec3e8f6b971276e3d07382691f22dbec0c
Jonas Limpert <33454605+JonaLimp@users.noreply.github.com>
2025-01-29 17:05:18 +0100

    Add logging

---
+++ src/logger.py
@@ -0,0 +1,27 @@
+import logging
+
+def setup_logger(log_file="pipeline.log"):
+    """
+    Set up a logger to write logs to both console and a file.
+    """
+    logger = logging.getLogger("RedditPipeline")
+    logger.setLevel(logging.INFO)
+
+    # Create file handler
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+
+    # Create console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    # Set log format
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    # Add handlers to logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
