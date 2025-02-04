f6e26b683d2a41c4a7da1de063e68e21d0f5505c
Jonas Limpert <33454605+JonaLimp@users.noreply.github.com>
2025-02-04 12:43:29 +0100

    Update .gitignore

--- src/logger.py
+++ src/logger.py
@@ -1,27 +1,29 @@
 import logging
 
-def setup_logger(log_file="pipeline.log"):
+def setup_logger(log_file="ecopulse.log"):
     """
     Set up a logger to write logs to both console and a file.
     """
-    logger = logging.getLogger("RedditPipeline")
-    logger.setLevel(logging.INFO)
+    logger = logging.getLogger("EcoPulse")
+    
+    if not logger.hasHandlers():
+        logger.setLevel(logging.INFO)
 
-    # Create file handler
-    file_handler = logging.FileHandler(log_file)
-    file_handler.setLevel(logging.INFO)
+        # Create file handler
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.INFO)
 
-    # Create console handler
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
+        # Create console handler
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
 
-    # Set log format
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-    file_handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
+        # Set log format
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
 
-    # Add handlers to logger
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
+        # Add handlers to logger
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
 
     return logger
